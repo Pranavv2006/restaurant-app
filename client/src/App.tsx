6821b8031dc79017a98c21c2c20485e7423bad98
Pranavv2006 <pranavvernekar023@gmail.com>
2025-08-11 23:35:53 +0530

    Added Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,13 @@
-import React from 'react';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./pages/landing";
 
 function App() {
   return (
-    <>
-      <div className="min-h-screen bg-blue-500 flex items-center justify-center">
-        <div className="bg-white p-8 rounded-lg shadow-lg">
-          <h1 className="text-2xl font-bold text-gray-800 text-center">
-            Pranav
-          </h1>
-          <p className="text-1xl text-pink-400">
-            Tailwind CSS is working
-          </p>
-        </div>
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing/>} />
+      </Routes>
+    </Router>
   )
 }
 
--- client/src/components/common/navbar.tsx
+++ client/src/components/common/navbar.tsx
@@ -1,1 +1,30 @@
-import react, {useState} from 'react';
+const Navbar = () => {
+    return (
+        <nav className="bg-white shadow-xl border-b border-orange-200 rounded mx-28 my-3">
+            <div className="max-w-7xl mx-auto px-4 sm:px-12 lg:px-24">
+                <div className="flex justify-between items-center h-16">
+                    
+                    {/* Title/Logo on the left */}
+                    <div className="flex items-center space-x-3">
+                        <span className="text-2xl">🍽️</span>
+                        <h1 className="text-xl font-bold text-gray-900">
+                            Tasty Bites
+                        </h1>
+                    </div>
+
+                    {/* Sign Up and Login on the right */}
+                    <div className="flex items-center space-x-4">
+                        <button className="text-gray-700 hover:text-orange-500 font-medium transition-colors">
+                            Login
+                        </button>
+                        <button className="bg-orange-500 hover:bg-white hover:border-2 hover:border-orange-500 hover:text-orange-500 text-white px-4 py-2 rounded-lg font-medium transition-colors duration-200">
+                            Sign Up
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
