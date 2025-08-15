7e498bbb3137b980886090c3a658f31ab68dfa20
Pranavv2006 <pranavvernekar023@gmail.com>
2025-08-16 01:17:51 +0530

    Added option to select roles in register modal

--- client/src/components/auth/Register.tsx
+++ client/src/components/auth/Register.tsx
@@ -1,8 +1,9 @@
 interface RegisterProps {
   onClose: () => void;
+  onRegisterClick?: () => void;
 }
 
-const Register = ({ onClose }: RegisterProps) => {
+const Register = ({ onClose, onRegisterClick }: RegisterProps) => {
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
             <div className="bg-white border border-gray-200 rounded-xl shadow-2xl max-w-lg w-full m-3 dark:bg-neutral-900 dark:border-neutral-800">
@@ -42,6 +43,54 @@ const Register = ({ onClose }: RegisterProps) => {
                                         required 
                                         placeholder="Enter your password"
                                     />
+                                </div>
+
+                                <div>
+                                    <label className="block text-sm mb-2 dark:text-white">First Name</label>
+                                    <input 
+                                        type="text" 
+                                        id="first-name" 
+                                        name="first-name" 
+                                        className="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-800 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" 
+                                        required 
+                                        placeholder="Enter your first name"
+                                    />
+                                </div>
+
+                                <div>
+                                    <label className="block text-sm mb-2 dark:text-white">Last Name</label>
+                                    <input 
+                                        type="text" 
+                                        id="last-name" 
+                                        name="last-name" 
+                                        className="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-800 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" 
+                                        required 
+                                        placeholder="Enter your last name"
+                                    />
+                                </div>
+
+                                <div className="items-center">
+                                    <h3 className="mb-4 font-semibold text-gray-900 dark:text-white">Role</h3>
+                                    <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                                        <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                            <div className="flex items-center ps-3">
+                                                <input id="horizontal-list-radio-license" type="radio" value="" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                                <label htmlFor="horizontal-list-radio-license" className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Customer</label>
+                                            </div>
+                                        </li>
+                                        <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                            <div className="flex items-center ps-3">
+                                                <input id="horizontal-list-radio-id" type="radio" value="" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                                <label htmlFor="horizontal-list-radio-id" className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Merchant</label>
+                                            </div>
+                                        </li>
+                                        <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                            <div className="flex items-center ps-3">
+                                                <input id="horizontal-list-radio-military" type="radio" value="" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                                <label htmlFor="horizontal-list-radio-military" className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">SuperAdmin</label>
+                                            </div>
+                                        </li>
+                                    </ul>
                                 </div>
 
                                 <button type="submit" className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
