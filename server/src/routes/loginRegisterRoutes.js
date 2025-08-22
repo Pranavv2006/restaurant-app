2794ce4de35d5b254852be765a751f06fa693f15
Pranavv2006 <pranavvernekar023@gmail.com>
2025-08-22 20:43:03 +0530

    Changes to routers for new controller imports

--- server/src/routes/loginRegisterRoutes.js
+++ server/src/routes/loginRegisterRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const loginRegisterController = require('../controllers/loginRegisterController');
-const forgotPassword = require('../controllers/ForgotPasswordController');
+const login = require('../controllers/loginController');
+const register = require('../controllers/registerController');
 
-router.post('/register', loginRegisterController.registerController); 
-router.post('/login', loginRegisterController.loginController);
-router.post('/forgot-password', forgotPassword.ForgotPasswordReset);
+router.post('/register', register.registerController);
+router.post('/login', login.loginController);
 module.exports = router;
