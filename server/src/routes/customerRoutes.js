3f802eec8961bffc7c3d8afff9cc2ee8ec90c479
Pranav <pranavvernekar023@gmail.com>
2025-09-19 22:45:45 +0530

    updated Restaurants data

--- server/src/routes/customerRoutes.js
+++ server/src/routes/customerRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const searchRestaurant = require("../controllers/searchRestaurant");
 
-// Add the search route
 router.get("/search-restaurants", searchRestaurant.searchRestaurantController);
 
 module.exports = router;
